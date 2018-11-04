0afc42caf2bf680a9cf61e66ea59d5b7350ba0b7
Sagar Modak <sagar.modak@wwindia.com>
2018-11-04 14:00:46 +0530

    base project setup

---
+++ seeFood/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  seeFood
+//
+//  Created by Sagar Modak on 04/11/18.
+//  Copyright © 2018 Sagar Modak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello")
+    }
+
+
+}
+
